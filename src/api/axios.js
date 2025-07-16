157aa3ed2a46653ec34b0e8679a067f43efb346c
Aryasudeep2024 <arya32.2011@gmail.com>
2025-07-16 15:04:17 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust if needed
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // ⬅️ important for cookie support
 });
 
